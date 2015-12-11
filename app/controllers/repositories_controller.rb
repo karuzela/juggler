ac032b31d64fd38ba1c6ef2544935753834d05da
Grzegorz Korzeniowski <g.korzeniowski@visuality.pl>
2015-12-11 13:05:29 +0100

    Create repositories index view
     - Add another actions to repositories controller

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,13 +1,29 @@
 class RepositoriesController < ApplicationController
+
+  before_action :set_repository, only: [:add, :remove]
+
   def index
+    @repositories = Repository.order(name: :asc).all
   end
 
   def add
+    SynchronizeGithubRepositoryService.new(@repository).call
+    redirect_to repositories_path, notice: 'Repository was synchronized'
   end
 
   def remove
+    UnsynchronizeGithubRepositoryService.new(@repository).call
+    redirect_to repositories_path, notice: 'Repository was unsynchronized'
   end
 
   def refresh
+    #GetGithubRepositoriesListService.new(current_user.github_access_token).call
+    redirect_to repositories_path, notice: 'Repositories list was refreshed'
+  end
+
+  private
+
+  def set_repository
+    @repository = Repository.find(params[:id])
   end
 end
