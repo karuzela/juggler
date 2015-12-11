ec11de9bd8bebaad0db85a6c96e9ac9a82505f96
Michał Młoźniak <m.mlozniak@gmail.com>
2015-12-11 13:22:34 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211121658) do
+ActiveRecord::Schema.define(version: 20151211121849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20151211121658) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.integer  "github_id"
+    t.string   "full_name"
   end
 
   add_index "repositories", ["github_id"], name: "index_repositories_on_github_id", using: :btree
