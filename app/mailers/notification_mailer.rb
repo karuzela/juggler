232cab15468c30935f060efaa32dc28680541cd7
Michał Młoźniak <m.mlozniak@gmail.com>
2015-12-11 18:13:20 +0100

    Remove blank line

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,4 @@
 class NotificationMailer < ApplicationMailer
-
   def auto_assign(pull_request)
     @pull_request = pull_request
     mail to: @pull_request.reviewer, subject: "New pull request to review"
