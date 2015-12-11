d2fc457804852f312fea5515df230e3077800baa
Grzegorz Korzeniowski <g.korzeniowski@visuality.pl>
2015-12-11 14:26:03 +0100

    Implement github hooks (un)subscription

---
+++ app/services/synchronize_github_repository_service.rb
@@ -0,0 +1,20 @@
+class SynchronizeGithubRepositoryService
+
+  def initialize(repo, access_token, callback, opts={})
+    @client = Octokit::Client.new(access_token: access_token)
+    @repo = repo
+    @callback = callback
+  end
+
+  def call
+    @client.subscribe(topic, @callback)
+    @repo.update_attribute :synchronized, true
+  end
+
+  private
+
+  def topic
+    @repo.html_url + '/events/pull_request'
+  end
+
+end
