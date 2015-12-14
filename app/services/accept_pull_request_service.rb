87502f3a7e7084ad6740f5b879c136633bf9aa0f
Grzegorz Korzeniowski <g.korzeniowski@visuality.pl>
2015-12-14 12:55:44 +0100

    Implement auto-adding comments to PR
    When user do review and click "accept" or "reject" button in Juggler
    send comment to GitHub Pull Request via github api
    
     - Add issue number field to Pull Request model

--- app/services/accept_pull_request_service.rb
+++ app/services/accept_pull_request_service.rb
@@ -1,7 +1,8 @@
 class AcceptPullRequestService
-  def initialize(pull_request, params)
+  def initialize(pull_request, params, user)
     @pull_request = pull_request
     @params = params
+    @client = Octokit::Client.new(access_token: user.github_access_token, auto_paginate: true)
   end
 
   def call
@@ -9,5 +10,10 @@ class AcceptPullRequestService
 
     @pull_request.state = PullRequestState::ACCEPTED
     @pull_request.save
+    send_comment_to_github if @pull_request.issue_number.present?
+  end
+
+  def send_comment_to_github
+    @client.add_comment(@pull_request.repository.full_name, @pull_request.issue_number, 'Accepted: ' + @pull_request.body)
   end
 end
