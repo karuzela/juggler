597126c69e06f559391ecf326c83fcfddbcd4cca
Grzegorz Korzeniowski <g.korzeniowski@visuality.pl>
2015-12-21 12:52:25 +0100

    Force user to integrate with Github

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def link_to_github_connect
+  def link_to_github_connect(opts={})
     gh_params = {
       client_id: ENV['GITHUB_APP_CLIENT_ID'],
       redirect_uri: github_connect_users_url,
@@ -8,11 +8,11 @@ module ApplicationHelper
     url = 'https://github.com/login/oauth/authorize?' + gh_params.to_query
 
     if current_user.connected_with_github?
-      link_to '#' do
+      link_to '#', opts do
         '<i class="fa fa-github"></i> Connected'.html_safe
       end
     else
-      link_to(url) do
+      link_to url, opts do
         '<i class="fa fa-github"></i> Connect with Github'.html_safe
       end
     end
