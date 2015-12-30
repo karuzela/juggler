a681cab7bb5eab7a124fbb5fc5ecdc3519795806
Grzegorz Korzeniowski <g.korzeniowski@visuality.pl>
2015-12-30 11:02:27 +0100

    Add GetGithubRepositoriesListService spec
     - Modify this service to update existing repos attributes
    
     - Update repositories factory

--- spec/factories/repositories.rb
+++ spec/factories/repositories.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
     sequence(:git_url) { |number| "git@github.com:username/project-name-#{number}.git" }
     sequence(:owner) { |number| "github-username-#{number}" }
     sequence(:full_name) { |number| "username/project-name-#{number}" }
+    sequence(:github_id) { |number| number }
     synchronized true
   end
 end
---
+++ spec/services/get_github_repositories_list_service_spec.rb
@@ -0,0 +1,26 @@
+describe GetGithubRepositoriesListService do
+  let(:service) { GetGithubRepositoriesListService.new(access_token: 'abcdef') }
+  let!(:repository) { create(:repository, github_id: 1) }
+  let(:repositories_list) { JSON.parse(File.open(Rails.root.to_s + '/spec/fixtures/github_repositories.json').read) }
+
+  before(:each) do
+    allow(Octokit::Client).to receive_message_chain('new.repositories').and_return(repositories_list)
+  end
+
+  it 'add 2 new repositories' do
+    expect { service.call }.to change { Repository.all.count }.by(2)
+  end
+
+  it 'update 1 existing repository' do
+    expected_attributes = {
+      'full_name' => 'username/repo1',
+      'name' => 'repo1',
+      'git_url' => 'git@github.com:username/repo1.git',
+      'html_url' => 'https://github.com/username/repo1',
+      'owner' => 'username'
+    }
+    expect { service.call }.to change { 
+      repository.reload.attributes.slice('full_name', 'name', 'git_url', 'html_url', 'owner') 
+    }.to(expected_attributes)
+  end
+end
