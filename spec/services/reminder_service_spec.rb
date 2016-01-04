22b12c2f045d6ddd84601b5ef163ee9bea15b7e0
Grzegorz Korzeniowski <g.korzeniowski@visuality.pl>
2016-01-04 15:49:33 +0100

    Add ReminderService spec

---
+++ spec/services/reminder_service_spec.rb
@@ -0,0 +1,26 @@
+describe ReminderService do
+  context 'on pending pull request' do
+    let(:pull_request) { create(:pull_request, state: PullRequestState::PENDING, reviewer: create(:user)) }
+    let(:service) { ReminderService.new(pull_request) }
+
+    before { stub_request(:post, /.*hooks.slack.com*/).to_return(status: 200) }
+
+    it 'sends slack message' do
+      expect(SlackClient).to receive_message_chain('new.send_message')
+      service.call
+    end
+
+    it 'sends email' do
+      expect { service.call }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
+
+  context 'on pull request with state other than pending' do
+    let(:closed_pull_request) { create(:pull_request, state: PullRequestState::CLOSED) }
+    let(:service) { ReminderService.new(closed_pull_request) }
+
+    it 'returns false' do
+      expect(service.call).to be false
+    end
+  end
+end
