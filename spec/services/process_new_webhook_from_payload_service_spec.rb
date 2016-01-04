599656ef0c46fbac70267e19610131b0c5df0777
Grzegorz Korzeniowski <g.korzeniowski@visuality.pl>
2016-01-04 11:55:33 +0100

    Add ProcessNewWebhookFromPayloadService spec

---
+++ spec/services/process_new_webhook_from_payload_service_spec.rb
@@ -0,0 +1,13 @@
+describe ProcessNewWebhookFromPayloadService do
+  let(:payload) { JSON.parse( File.open(Rails.root.to_s + '/spec/fixtures/new_webhook_payload.json').read ) }
+  let(:service) { ProcessNewWebhookFromPayloadService.new(payload) }
+
+  it 'updates synchronized flag to true' do
+    repository = create(:repository, github_id: 1, synchronized: false)
+    expect { service.call }.to change { repository.reload.synchronized }.from(false).to(true)
+  end
+
+  it 'returns nil if repository is not found in db' do
+    expect(service.call).to be_nil
+  end
+end
